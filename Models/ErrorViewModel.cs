839771838970b73b956dbe2e4d2dbacbb917ccb6
Coppermine-SP <son5121502@gmail.com>
2023-07-24 02:55:20 +0900

    create asp.net project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cloudinteractive_statuspage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
