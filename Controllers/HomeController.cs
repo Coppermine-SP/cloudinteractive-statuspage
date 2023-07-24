857ceef456d998fa207277ed61796ad4deacc532
Coppermine-SP <son5121502@gmail.com>
2023-07-24 21:20:39 +0900

    기본 layout 및 alert 작업

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,10 @@ namespace cloudinteractive_statuspage.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            var Testmodel = new DashboardModel();
+            Testmodel.NotifyList.Add(new NotifyItem(NotifyItem.NotifyType.Info, "새로운 클라우드인터렉티브 C3 리전에 대해 자세히 알아보십시오."));
+            Testmodel.NotifyList.Add(new NotifyItem(NotifyItem.NotifyType.Warn, "한국 동남부지역 기상 악화에 따른 접속 지연 문제 해결 중. (C1)"));
+            return View(Testmodel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/DashboardModel.cs
@@ -0,0 +1,26 @@
+﻿namespace cloudinteractive_statuspage.Models
+{
+    public class DashboardModel
+    {
+        public List<NotifyItem> NotifyList = new List<NotifyItem>();
+    }
+
+    public struct NotifyItem
+    {
+        public enum NotifyType
+        {
+            Info,
+            Warn,
+            Error
+        };
+
+        public NotifyType Type;
+        public string Content;
+
+        public NotifyItem(NotifyType type, string content)
+        {
+            Type = type;
+            Content = content;
+        }
+    }
+}
