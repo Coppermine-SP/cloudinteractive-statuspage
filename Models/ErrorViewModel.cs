af95e520da6b145b5532bb722574f82767f462ed
Coppermine-SP <son5121502@gmail.com>
2023-07-25 20:20:18 +0900

    하단 layout 작업 및 code annotation.

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace cloudinteractive_statuspage.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
